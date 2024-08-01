73d1a9baba75fe3b4604028ef0cc51129c812a3e
william.alves <alvesrez@gmail.com>
2024-08-01 19:37:17 -0300

    first commit

---
+++ src/main/java/br/iftm/edu/prova_william/ProvaWilliamApplication.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.prova_william;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaWilliamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaWilliamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/iftm/edu/prova_william/ProvaWilliamApplicationTests.java
@@ -0,0 +1,13 @@
+package br.iftm.edu.prova_william;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaWilliamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
